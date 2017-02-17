d0d3273fccab22579def4316f7ccbaf51cab840c
advanceboy.dolphin <advanceboy.dolphin@gmail.com>
2017-02-18 03:15:17 +0900

    added development environment

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var ts = require('gulp-typescript');
+
+gulp.task('compress jsqrcode', function () {
+    return gulp.src([
+            "src/jsqrcode/src/grid.js",
+            "src/jsqrcode/src/version.js",
+            "src/jsqrcode/src/detector.js",
+            "src/jsqrcode/src/formatinf.js",
+            "src/jsqrcode/src/errorlevel.js",
+            "src/jsqrcode/src/bitmat.js",
+            "src/jsqrcode/src/datablock.js",
+            "src/jsqrcode/src/bmparser.js",
+            "src/jsqrcode/src/datamask.js",
+            "src/jsqrcode/src/rsdecoder.js",
+            "src/jsqrcode/src/gf256poly.js",
+            "src/jsqrcode/src/gf256.js",
+            "src/jsqrcode/src/decoder.js",
+            "src/jsqrcode/src/QRCode.js",
+            "src/jsqrcode/src/findpat.js",
+            "src/jsqrcode/src/alignpat.js",
+            "src/jsqrcode/src/databr.js",
+        ])
+        .pipe(concat('jsqrcode-combined.js'))
+        .pipe(uglify())
+        .pipe(rename('jsqrcode-combined.min.js'))
+        .pipe(gulp.dest('lib/'));
+});
+
+gulp.task('compress jquery-qrcode-reader', function () {
+    var tsProject = ts.createProject('tsconfig.json');
+    return gulp.src('src/webrtc-qrcode-reader.ts')
+        .pipe(tsProject())
+        .pipe(uglify())
+        .pipe(rename('webrtc-qrcode-reader.min.js'))
+        .pipe(gulp.dest('lib/'));
+});
+
+gulp.task('compress', ['compress jsqrcode', 'compress jquery-qrcode-reader']);
---
+++ src/webrtc-qrcode-reader.ts
@@ -0,0 +1,10 @@
+namespace WebRtcQR {
+    export function start_reader() {
+        // start the capturing
+        ;
+    }
+    export function stop_reader() {
+        // stop the capturing
+        ;
+    }
+}
